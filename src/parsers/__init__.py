ade12c1babd5ff176a55e02e79506efc6eacae04
Max <milk333445@gmail.com>
2026-05-24 22:54:06 +0800

    feat: 移除 TocAnchorParser，簡化解析器結構

--- src/parsers/__init__.py
+++ src/parsers/__init__.py
@@ -1,7 +1,6 @@
 from src.parsers.base import BaseParser, ParseResult
 from src.parsers.cross_reference_multispan_parser import CrossReferenceMultiSpanParser
 from src.parsers.regex_parser import RegexParser
-from src.parsers.toc_anchor_parser import TocAnchorParser
 from src.parsers.llm_parser import LLMParser
 from src.parsers.hybrid import HybridParser
 
@@ -10,7 +9,6 @@ __all__ = [
     "ParseResult",
     "CrossReferenceMultiSpanParser",
     "RegexParser",
-    "TocAnchorParser",
     "LLMParser",
     "HybridParser",
 ]
